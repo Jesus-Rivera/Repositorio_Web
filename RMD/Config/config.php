9bd542efb0d0c83059faee1959d42f0f1c8205a8
Jesus <jesusrivera55.jr@gmail.com>
2019-10-09 10:50:29 -0500

    Configuraciones principales

---
+++ RMD/Config/config.php
@@ -0,0 +1,8 @@
+<?php 
+	define('URL','http://localhost/RepWeb/Repositorio_Web/RMD/');
+	define('HOST','localhost');
+	define('DB','Repositorio');
+	define('User','root');
+	define('Password','');
+	define('CHARSET','utf8mb4');
+ ?>
