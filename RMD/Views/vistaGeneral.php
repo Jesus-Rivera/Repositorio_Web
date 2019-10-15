2a3b7f6f01a6d1421de997e3785883b3edd7130a
Enrique S.Franco <quique_arcenal@hotmail.com>
2019-10-15 16:22:40 -0500

    nuevo navbar

--- RMD/Views/vistaGeneral.php
+++ RMD/Views/vistaGeneral.php
@@ -1,142 +1,208 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="es">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<title>RMD</title>
-	<!-- estilos propios -->
-	<link rel="stylesheet" href="css/navbarInicioSesion.css">
-	<link rel="stylesheet" href="css/mainInicioSesion.css">
-	
-	<!-- bootstrap -->
-  	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-  	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link href="css/bootstrap.css" rel="stylesheet" />
 
-	<!-- jquery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link href="css/pe-icon-7-stroke.css" rel="stylesheet" />
+	<link href="css/ct-navbar.css" rel="stylesheet" />
 
-	<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
 
-	<!-- js -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script src="js/navbarSesion.js"></script>
+	<!--     Font Awesome     -->
+	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+	<link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+	<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
+
 </head>
-<body class="hero-anime">	
-	<!-- navbar -->
-    <div class="navigation-wrap bg-light start-header start-style">
-    	<div class="container">
-    		<div class="row">
-    			<div class="col-12">
-    				<nav class="navbar navbar-expand-md navbar-light">
-    					<a class="navbar-brand" href="#" target="_blank"><img src="img/logo.png" alt=""></a>
-    					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    						<span class="navbar-toggler-icon"></span>
-    					</button>
 
-    					<div class="collapse navbar-collapse" id="navbarSupportedContent">
-							<form class="form-inline my-2 my-lg-0">
-								<input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-								<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-							</form>
-    						<ul class="navbar-nav ml-auto py-4 py-md-0">
-    							<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-    								<a class="nav-link" href="#">Inicio</a>
-    							</li>
-    							<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-    								<a class="nav-link" href="#">Portfolio</a>
-    							</li>
-    							<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-    								<a class="nav-link" href="#">Agency</a>
-    							</li>
-    							
-    							<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-    								<a class="nav-link" href="#">Journal</a>
-    							</li>
-    							<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-									<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Configuración</a>
-									<div class="dropdown-menu">
-										<a class="dropdown-item" href="#">Perfil</a>
-										<a class="dropdown-item" href="#">Modo Dark</a>
-										<a class="dropdown-item" href="#">Cerrar Sesión</a>
-									</div>
-								</li>
-    						</ul>
-    					</div>
+<body>
+	<div id="navbar-full">
+		<div id="navbar">
+			<nav class="navbar navbar-ct-blue navbar-fixed-top navbar-transparent" role="navigation">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse"
+							data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand navbar-brand-logo" href="#">
+							<div class="logo">
+								<img src="images/user.png" style="width: 65px;">
+							</div>
+							<div class="brand">Avatar: user1</div>
+						</a>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li>
+								<a href="javascript:void(0);" data-toggle="search" class="hidden-xs">
+									<i class="pe-7s-search"></i>
+									<p>Buscar</p>
+								</a>
+							</li>
 
-    				</nav>
-    			</div>
-    		</div>
-    	</div>
-	</div>
-	<!-- visualizar archivos -->
-	<div class="wrapper">
-		<section id="section1">
-			<a href="#section3" class="arrow__btn">‹</a>
-			<div class="item">
-				<img src="https://occ-0-1567-1123.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABRvngexxF8H1-OzRWFSj6ddD-aB93tTBP9kMNz3cIVfuIfLEP1E_0saiNAwOtrM6xSOXvoiSCMsihWSkW0dq808-R7_lBnr6WHbjkKBX6I3sD0uCcS8kSPbRjEDdG8CeeVXEAEV6spQ.webp" />
-			</div>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABZEK-7pZ1H5FD4cTyUb9qB_KeyJGz5p-kfPhCFv4GU_3mbdm8Xfsy4IBchlG9PFNdGff8cBNPaeMra72VFnot41nt0y3e8RLgaVwwh3UvyM2H2_MkmadWbQUeGuf811K7-cxJJh7gA.jpg" />
-			</div>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABQCoK53qihwVPLRxPEDX98nyYpGbxgi5cc0ZOM4iHQu7KQvtgNyaNM5PsgI0vy5g3rLPZdjGCFr1EggrCPXpL77p2H08jV0tNEmIfs_e8KUfvBJ6Ay5nM4UM1dl-58xA6t1swmautOM.webp" />
-			</div>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABdYtAqj8CyaJTWq5taD8Ro_UgwH3nne9QpFGVps-2J3IG-leqrfqXFii4jzZn48nPYTkrlwKQEV0R7_cEKlYBPRzdKqNODc-Oz26IL3LlLgFboXibIWXwxzeYxzuqn0I9TpARjeByw.jpg" />
-			</div>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABbcCX42tsqGbBvO2y9CQv5-7QvYbCfoHtXsuc6NPCtZaKa4l4fBX3XWvUwG9F2A3CTsNpHVmulxBbdXKwK8Q6xGjejd9FoadGkZ7CnGrSl601TOQjzSHJ23NuIPC8j0QMGORL4uRIA.jpg" />
-			</div>
-			<a href="#section2" class="arrow__btn">›</a>
-		</section>
-		<section id="section2">
-			<a href="#section1" class="arrow__btn">‹</a>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABVopDZ5Fy9_fk_HO5WxHTXKKjKhtWFupbSjuvPwfLK_vytzon4EwRUdGgYJ34JwPxOTK_NkV3aXfkULMB0Dcct-FyfqzH-X44VXuRMp4QeBHlvKwWeZFpZlGdItPzmmg4scmwhG7SQ.jpg" />
-			</div>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABTOj1-116yVcgKWMU2dI3GFR4x0fSkiGsqtLLeLUxRR7STaksjAqBTrYlTfrB8nIGnGvXksi0ewXAhVGg6-pLxpFOIfcpjK-pf8D5xehFZo5a6vJbo4L0AGbrzglbyUoq255QBJgRQ.jpg" />
-			</div>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABd3IBDpxbRcHXvRMFCZeKa2aHLU1P4SJtrACMS9om3yhLjqPlvNlmR_fypPxjtbsbnKaC4JZhPSpDG4r_kdxSHHAltWguMcCB-1Y1OShr2zWfUv7Whf_39fNH5ZJ3_0gxQrs0akmQjQz44_LT7jXH5LMZ7iMBAzac5IEj4m7Fn_5OWEGYnVsDsKG-QTommDooULMDF9bEw.jpg" />
-			</div>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABXSd7bhDddcwkq9XpksoQFCHVx29Sxl_h4hb2n3F2GIt32a4XWcOnctQfgnT5qdolv8UML6_xNB5CJ89h56wueb13mYmEBr0sx5e9iLPdtVcOQAOmKXKWHHXwFvJuCUwuNnL3s8eAQwqLXPVMHMEsujM684rUGrZNF2btN2GRy5-RyEslsxZO93V2Q_H2bWs8A8oayt1h5M.webp" />
-			</div>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABbXWODpAWqVXcmmjMA7K-2mPkQpvwCLfSdeyhVXzR8A3MSpdSEnnjf4HEJJTYC-TnktU6njTUGAxmzWEYCaJbk4v_ZeL-7QGzmkvYBjg_N-evr2XmcX-Fanoyvu_nimFP4iigPe4O3Vr_WcgplhwkDrJwPUJa84wRLrNAx3TufN5V7cWRP4indqu5HQahvgFEqfL9zjp4g.jpg" />
-			</div>
-			<a href="#section3" class="arrow__btn">›</a>
-		</section>
-		<section id="section3">
-			<a href="#section2" class="arrow__btn">‹</a>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABRr4YxdaABuAuH_3FmSQZn7BCvLp-UUPsXE9MiYpvFP3CSUHV2zOew5oVqKqqdaOd3tbFVS0Uf67uIs7_eZydlCghg4nK0nMatRpPImABwTOhnNzCLCxdKrua7pPIcPCZqBYTeAO5g.jpg" />
-			</div>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABTyWK1MKaw8GcObtz47R2Tj7wkLJ7qQu9tk6TVpcoyxpzD4B-zZ569bQ5vGrREBL-MWFkGilXUwy7tCDaj2XOGkUB4RsbbFAmp9NgSr6lygMpUGNHSlyfrFbUORsRkrxSIoh_ggOvg.jpg" />
-			</div>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABY7NwkWEJIfXsn6t3Li4bGSCQ1nEErPisI5ZZtXlC-_VRBZOUrhWK5X3vt3t6SR_cpgVhCwxgQqFFDJhk62Kk8hawOnYGZMr0LKeLczMFV2zalCFjkcdLksvT4HB2LEi6LFyruyk3Uu0LmNGsHfC2A8Bly60smr_3sDbz4RruXcklPOG1qYq9wUVu3zfaiwNvqmG4b8aFA.jpg" />
-			</div>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABemXHOga9feFnOux6I2YyACBD94wvM7N3vcTGIfMpQ_BcaXeoeM9XyzdVdamKtxt0SHXZfvsl6czcp3E48tXMLtHBxuQsh1BjHtPGgJDZ81je_FjItINiqzLtir0A30s_e4KR8G3d7AYAPDjZVOY97bNpzNqtkcHcGp7fGnJByVCps1uLfG9U9tK3Ma1A_3JbRt0NiT2_Q.jpg" />
-			</div>
-			<div class="item">
-				<img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABVxuRB932hvre-XP0gh6ar5ztoR3Oe3QjKHkyvcDnRak2MKXOrx5H7mFQSvggefMFOppwEs7ZCCpiqrJ_CYGvtvYB9NpU4SWUtNO6uV2u-DTID267AcHjHcGvGBQJ1ufddDkxcGOZyi5MlOQ5QUmBun4652FbYUnib3zMYQDgcna_Pvz8y_HO5fbokxezrRR1JZAAiqFSQ.jpg" />
-			</div>
-			<a href="#section1" class="arrow__btn">›</a>
-		</section>
-	</div>
+							<li>
+								<a href="#">
+									<i class="pe-7s-mail">
+										<!-- numero de mensajes -->
+										<span class="label">0</span>
+									</i>
+									<p>Mensajes</p>
+								</a>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+									<i class="pe-7s-user"></i>
+									<p>Configuración<b class="caret"></b></p>
+								</a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Perfil</a></li>
+									<li class="divider"></li>
+									<li><a href="#">Cerrar Sesión</a></li>
+								</ul>
+							</li>
+						</ul>
+						<form class="navbar-form navbar-right navbar-search-form" role="search">
+							<div class="form-group">
+								<input type="text" value="" class="form-control" placeholder="Search...">
+							</div>
+						</form>
 
-	
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js">
-	</script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js">
-	</script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
-	</script>
-  </body>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+			<div class="blurred-container">
+				<div class="img-src" style="background-image: url('images/bg.jpg')"></div>
+			</div>
+		</div><!--  fin navbar -->
+	</div> <!-- fin del menu -->
+
+	<section id="mas-visto" style="padding-top: 110px;">
+		<div class="container-fluid">
+			<h1 class="text-center my-3">MAS VISTOS</h1>
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<div class="carousel-inner row w-100 mx-auto">
+					<div class="carousel-item col-md-4 active">
+						<div class="card">
+							<div class="card-body">
+								<h4 class="card-title">Card 1</h4>
+								<p class="card-text">This is a longer card with supporting text below as a natural
+									lead-in to additional content. This content is
+									a little bit longer.</p>
+								<p class="card-text">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="carousel-item col-md-4">
+						<div class="card">
+							<div class="card-body">
+								<h4 class="card-title">Card 2</h4>
+								<p class="card-text">This is a longer card with supporting text below as a natural
+									lead-in to additional content. This content is
+									a little bit longer.</p>
+								<p class="card-text">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="carousel-item col-md-4">
+						<div class="card">
+							<div class="card-body">
+								<h4 class="card-title">Card 3</h4>
+								<p class="card-text">This is a longer card with supporting text below as a natural
+									lead-in to additional content. This content is
+									a little bit longer.</p>
+								<p class="card-text">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="carousel-item col-md-4">
+						<div class="card">
+							<div class="card-body">
+								<h4 class="card-title">Card 4</h4>
+								<p class="card-text">This is a longer card with supporting text below as a natural
+									lead-in to additional content. This content is
+									a little bit longer.</p>
+								<p class="card-text">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="carousel-item col-md-4">
+						<div class="card">
+							<div class="card-body">
+								<h4 class="card-title">Card 5</h4>
+								<p class="card-text">This is a longer card with supporting text below as a natural
+									lead-in to additional content. This content is
+									a little bit longer.</p>
+								<p class="card-text">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="carousel-item col-md-4">
+						<div class="card">
+							<div class="card-body">
+								<h4 class="card-title">Card 6</h4>
+								<p class="card-text">This is a longer card with supporting text below as a natural
+									lead-in to additional content. This content is
+									a little bit longer.</p>
+								<p class="card-text">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="carousel-item col-md-4">
+						<div class="card">
+							<div class="card-body">
+								<h4 class="card-title">Card 7</h4>
+								<p class="card-text">This is a longer card with supporting text below as a natural
+									lead-in to additional content. This content is
+									a little bit longer.</p>
+								<p class="card-text">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-12 text-center mt-4">
+							<a class="btn btn-outline-secondary mx-1 prev" href="javascript:void(0)" title="Previous">
+								<i class="fa fa-lg fa-chevron-left"></i>
+							</a>
+							<a class="btn btn-outline-secondary mx-1 next" href="javascript:void(0)" title="Next">
+								<i class="fa fa-lg fa-chevron-right"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="js/jquery-1.10.2.js" type="text/javascript"></script>
+	<script src="js/bootstrap.js" type="text/javascript"></script>
+	<script src="js/ct-navbar.js"></script>
+
+</body>
+
 </html>
