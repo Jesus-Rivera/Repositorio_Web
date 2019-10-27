5e00b2b1184040a8e1d461b4d20753cd5b119fd1
Enrique S.Franco <quique_arcenal@hotmail.com>
2019-10-27 00:26:51 -0500

    js y css

--- RMD/Resources/js/main.js
+++ RMD/Resources/js/main.js
@@ -1,68 +1,63 @@
 $(document).ready(function(){
-    $('.tooltips-general').tooltip('hide');
-    $('.mobile-menu-button').on('click', function(){
-        var mobileMenu=$('.navbar-lateral');	
-        if(mobileMenu.css('display')=='none'){
-            mobileMenu.fadeIn(300);
+	/*Mostrar ocultar area de notificaciones*/
+	$('.btn-Notification').on('click', function(){
+        var ContainerNoty=$('.container-notifications');
+        var NotificationArea=$('.NotificationArea');
+        if(NotificationArea.hasClass('NotificationArea-show')&&ContainerNoty.hasClass('container-notifications-show')){
+            NotificationArea.removeClass('NotificationArea-show');
+            ContainerNoty.removeClass('container-notifications-show');
         }else{
-            mobileMenu.fadeOut(300);
+            NotificationArea.addClass('NotificationArea-show');
+            ContainerNoty.addClass('container-notifications-show');
         }
     });
-    $('.dropdown-menu-button').on('click', function(){
-        var dropMenu=$(this).next('ul');
-        dropMenu.slideToggle('slow');
+    /*Mostrar ocultar menu principal*/
+    $('.btn-menu').on('click', function(){
+    	var navLateral=$('.navLateral');
+    	var pageContent=$('.pageContent');
+    	var navOption=$('.navBar-options');
+    	if(navLateral.hasClass('navLateral-change')&&pageContent.hasClass('pageContent-change')){
+    		navLateral.removeClass('navLateral-change');
+    		pageContent.removeClass('pageContent-change');
+    		navOption.removeClass('navBar-options-change');
+    	}else{
+    		navLateral.addClass('navLateral-change');
+    		pageContent.addClass('pageContent-change');
+    		navOption.addClass('navBar-options-change');
+    	}
     });
-    $('.exit-system-button').on('click', function(e){
-        e.preventDefault();
-        var LinkExitSystem=$(this).attr("data-href");
-        swal({
-            title: "¿Estás seguro?",
-            text: "Quieres salir del sistema y cerrar la sesión actual",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#5cb85c",
-            confirmButtonText: "Si, salir",
-            cancelButtonText: "No, cancelar",
-            animation: "slide-from-top",
-            closeOnConfirm: false 
-        },function(){
-            window.location=LinkExitSystem; 
-        });  
+    /*Salir del sistema*/
+    $('.btn-exit').on('click', function(){
+    	swal({
+		  	title: 'You want out of the system?',
+		 	text: "The current session will be closed and will leave the system",
+		  	type: 'warning',
+		  	showCancelButton: true,
+		  	confirmButtonText: 'Yes, exit',
+		  	closeOnConfirm: false
+		},
+		function(isConfirm) {
+		  	if (isConfirm) {
+		    	window.location='index.html'; 
+		  	}
+		});
     });
-    $('.search-book-button').click(function(e){
-        e.preventDefault();
-        var LinkSearchBook=$(this).attr("data-href");
-        swal({
-           title: "¿Qué libro estás buscando?",
-           text: "Por favor escribe el nombre del libro",
-           type: "input",   
-           showCancelButton: true,
-           closeOnConfirm: false,
-           animation: "slide-from-top",
-           cancelButtonText: "Cancelar",
-           confirmButtonText: "Buscar",
-           confirmButtonColor: "#3598D9",
-           inputPlaceholder: "Escribe aquí el nombre de libro" }, 
-      function(inputValue){
-           if (inputValue === false) return false;  
-
-           if (inputValue === "") {
-               swal.showInputError("Debes escribir el nombre del libro");     
-               return false;   
-           } 
-            window.location=LinkSearchBook+"?bookName="+inputValue;
-       });
-    });
-    $('.btn-help').on('click', function(){
-        $('#ModalHelp').modal({
-            show: true,
-            backdrop: "static"
-        });
+    /*Mostrar y ocultar submenus*/
+    $('.btn-subMenu').on('click', function(){
+    	var subMenu=$(this).next('ul');
+    	var icon=$(this).children("span");
+    	if(subMenu.hasClass('sub-menu-options-show')){
+    		subMenu.removeClass('sub-menu-options-show');
+    		icon.addClass('zmdi-chevron-left').removeClass('zmdi-chevron-down');
+    	}else{
+    		subMenu.addClass('sub-menu-options-show');
+    		icon.addClass('zmdi-chevron-down').removeClass('zmdi-chevron-left');
+    	}
     });
 });
 (function($){
     $(window).load(function(){
-        $(".custom-scroll-containers").mCustomScrollbar({
+        $(".navLateral-body, .NotificationArea, .pageContent").mCustomScrollbar({
             theme:"dark-thin",
             scrollbarPosition: "inside",
             autoHideScrollbar: true,
